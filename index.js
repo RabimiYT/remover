c0e7feb978fa12dbe01e34bc13e5b6c53886032e
RabimiYT <fakerabimi@gmail.com>
2024-12-08 10:47:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["ラビミだよ", "製作者: ラビミ"];
-const statusTypes = [ 'offline', 'idle'];
+const statusTypes = [ 'Discord iOS', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
